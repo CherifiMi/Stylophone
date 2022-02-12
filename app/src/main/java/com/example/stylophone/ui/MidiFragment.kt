35e48115de0f149331054c89eb1f7d71ae43bc9d
CherifiMi <cherifi154949@gmail.com>
2022-02-12 20:29:34 +0100

    play note from sine fs

--- app/src/main/java/com/example/stylophone/ui/MidiFragment.kt
+++ app/src/main/java/com/example/stylophone/ui/MidiFragment.kt
@@ -1,5 +1,7 @@
 package com.example.stylophone.ui
 
+import android.R.attr
+import android.media.AudioFormat
 import android.media.JetPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,24 +11,83 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import com.example.stylophone.R
+import android.media.AudioTrack
+
+import android.media.AudioManager
+import android.view.MotionEvent
+
+import android.R.attr.button
+import android.annotation.SuppressLint
+import android.view.View.OnTouchListener
+
 
 class MidiFragment : Fragment() {
 
+    //--------------------values
+    var isPlaying: Boolean = false
+    lateinit var Track: AudioTrack
+    val Fs: Int = 44100
+    val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_midi, container, false)
 
-        view.findViewById<Button>(R.id.button).setOnClickListener{
-            Toast.makeText(requireContext(), "RRRRRRRRRRRRRRRR", Toast.LENGTH_SHORT).show()
+        val note = view.findViewById<Button>(R.id.button)
 
-            
-
-        }
 
 
         return view
     }
+
+    private fun initTrack() {
+        // Very similar to opening a stream in PyAudio
+        // In Android create a AudioTrack instance and initialize it with different parameters
+
+        // AudioTrack is deprecated for some android versions
+        // Please look up for other alternatives if this does not work
+        Track = AudioTrack(
+            AudioManager.MODE_NORMAL, Fs, AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT, buffLength, AudioTrack.MODE_STREAM
+        )
+    }
+
+    private fun playback(i: Int) {
+        // simple sine wave generator
+        val frame_out = ShortArray(buffLength)
+        val amplitude = 32767
+        val frequency = i
+        val twopi: Double = 8.0 * Math.atan(1.0)
+        var phase: Double = 0.0
+
+        while (isPlaying) {
+            for (i in 0 until buffLength) {
+                frame_out[i] = (amplitude * Math.sin(phase)).toInt().toShort()
+                phase += twopi * frequency / Fs
+                if (phase > twopi) {
+                    phase -= twopi
+                }
+            }
+            Track.write(frame_out, 0, buffLength)
+        }
+    }
+
+    private fun startPlaying() {
+        Track.play()
+        isPlaying = true
+    }
+
+    private fun stopPlaying() {
+        if (isPlaying) {
+            isPlaying = false
+            // Stop playing the audio data and release the resources
+            Track.stop()
+            Track.release()
+        }
+    }
+
 
 }
